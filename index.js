50461f5dda74cf10d008f704e64b33169814c2f8
rizky2330 <rizky.lazuardi23@gmail.com>
2022-04-11 12:17:34 +0700

    solve conflict

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const fs = require("fs");
+const https = require("https");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -16,7 +18,6 @@ app.get("/", (req, res) => {
 app.use("/products", productsRouter);
 
 app.use((error, req, res, next) => {
-  console.log(error);
   res.status(500).send({
     status: "ERROR",
     message: error.message,
@@ -24,8 +25,21 @@ app.use((error, req, res, next) => {
   });
 });
 
-app.listen(port, (err) => {
-  if (err) return cosole.log({ err });
-
-  console.log(`Api is running at port ${port}`);
-});
+if (process.env.NODE_ENV == "production") {
+  https
+    .createServer(
+      {
+        key: fs.readFileSync("server.key"),
+        cert: fs.readFileSync("server.cert"),
+      },
+      app
+    )
+    .listen(port, () => {
+      console.log(`Listening at ${port}`);
+    });
+} else {
+  app.listen(port, (err) => {
+    if (err) return cosole.log({ err });
+    console.log(`Api is running at port ${port}`);
+  });
+}
